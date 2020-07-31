d492a22df2b2f52c54e4a46858c924fecc6890cd
CitizenJoseph <65048244+CitizenJoseph@users.noreply.github.com>
2020-07-31 15:16:27 -0400

    first project from CC

---
+++ project1/initials.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+
+  std::cout << "JJJJJ  V   V   CCC\n";
+  std::cout << "  J    V   V  C   C\n";
+  std::cout << "  J    V   V  C\n";
+  std::cout << "J J    V   V  C\n";
+  std::cout << "J J     V V   C\n";
+  std::cout << "J J     V V   C   C\n";
+  std::cout << " J       V     CCC\n";
+
+  return 0;
+}
